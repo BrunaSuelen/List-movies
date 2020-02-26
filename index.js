9005ea61afb15dde7cc1341f1ee9cfee85708889
BrunaSuelen <>
2020-02-26 14:08:36 -0300

    Estruturação do template e setando conteúdo em ejs.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var request = require('request');
+var ejs = require('ejs');
 
 request('https://api.themoviedb.org/3/discover/movie?api_key=05db07e27024bc56a1e3aa80f74fc6bd', {json:true}, function (error, response, body) {
   if(error){
     console.log('Erro: ',error);
+
+
   }else{
     var listMovies = body.results.map(function (movie) {      
       return { title: movie.title, overview: movie.overview};
     });
-    console.log("Results:", listMovies);
+        
+    ejs.renderFile("./views/template.ejs", {listMovies}, function(err, str){
+      console.log("HTML:", str );
+    });
   }
+
+
 });
 
 
